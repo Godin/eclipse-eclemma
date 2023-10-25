06caae4039263a6d160545abfc1ce9336ce63c25
Evgeny Mandrikov <mandrikov@gmail.com>
2023-10-25 16:11:14 +0200

    (WIP) SWT screenshot

--- org.eclipse.eclemma.ui.test/src/org/eclipse/eclemma/ui/MenuTest.java
+++ org.eclipse.eclemma.ui.test/src/org/eclipse/eclemma/ui/MenuTest.java
@@ -20,12 +20,50 @@ import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.junit.After;
 import org.junit.Test;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
 public class MenuTest {
 
   private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
   @After
   public void resetWorkbench() {
+      // TODO seems that this doesn't work for Retina
+      new SWTWorkbenchBot().captureScreenshot("/tmp/screen.png");
+
+      // TODO seems that this works for single monitor
+      final Display display = Display.getDefault();
+      display.syncExec(new Runnable() {
+			public void run() {
+				GC gc = new GC(display);
+				Image image = null;
+
+				try {
+					image = new Image(display, display.getBounds().width, display.getBounds().height);
+					gc.copyArea(image, display.getBounds().x, display.getBounds().y);
+
+					ImageLoader imageLoader = new ImageLoader();
+					imageLoader.data = new ImageData[] { image.getImageData(200) };
+					imageLoader.save("/tmp/screen2.png", SWT.IMAGE_PNG);
+				} catch (Exception ex) {
+				} finally {
+					gc.dispose();
+					if (image != null) {
+						image.dispose();
+					}
+				}
+			}
+		});
+
     bot.resetWorkbench();
   }
 
